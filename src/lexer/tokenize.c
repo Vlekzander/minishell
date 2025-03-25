4844f19307009ee2f69cdc7b28e4f0ae9f18ed29
Vlekzander <alexandrepierret@protonmail.com>
2025-03-25 18:56:33 +0100

    Implementing tokenization basic quotes

--- src/lexer/tokenize.c
+++ src/lexer/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 23:23:36 by apierret          #+#    #+#             */
-/*   Updated: 2025/03/25 18:48:56 by apierret         ###   ########.fr       */
+/*   Updated: 2025/03/25 18:54:31 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,36 @@ static void	add_token(t_list **list, t_token_type type, char *value)
 		*value = '\0';
 }
 
+static void	process_group(char c, char *group)
+{
+	if (group == NULL)
+		return ;
+	if (*group == 0)
+		*group = c;
+	else if (*group == c)
+		*group = 0;
+}
+
 t_error	tokenize(t_list **tokens, char *input)
 {
 	char	*buf;
 	size_t	i;
 	size_t	j;
+	char	group;
 
 	if (tokens == NULL || input == NULL)
 		return (ERR_IMPLEMENTATION);
 	buf = ft_calloc(ft_strlen(input) +1, sizeof(char));
 	if (buf == NULL)
 		return (ERR_ALLOCATION);
+	group = 0;
 	i = 0;
 	j = 0;
 	while (input[i] != '\0')
 	{
-		if (ft_strchr(" \t\n", input[i]))
+		if (ft_strchr("'\"", input[i]) && (group == 0 || group == input[i]))
+			process_group(input[i], &group);
+		else if (group == 0 && ft_strchr(" \t\n", input[i]))
 		{
 			j = 0;
 			add_token(tokens, TK_WORD, buf);
