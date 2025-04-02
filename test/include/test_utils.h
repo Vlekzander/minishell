9a666f01b6f340b01eb372ceab0f4b3095bde56e
Alexandre Pierret <apierret@student.s19.be>
2025-04-02 12:03:56 +0200

    add AST comparison

--- test/include/test_utils.h
+++ test/include/test_utils.h
@@ -6,14 +6,16 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 23:20:26 by apierret          #+#    #+#             */
-/*   Updated: 2025/03/25 13:55:57 by apierret         ###   ########.fr       */
+/*   Updated: 2025/04/02 11:40:12 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEST_UTILS_H
 # define TEST_UTILS_H
+# include "data.h"
 # include "libft.h"
 
+int		ast_equal(t_ast *a, t_ast *b);
 int		lst_equal(t_list *a, t_list *b, int (*cmp)(void *, void *));
 int		token_equal(void *a, void *b);
 t_list	*create_token_list(char **strings);
