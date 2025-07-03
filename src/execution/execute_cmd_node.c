88ce8e73581850945c3ae22bdae85c899917473b
Vlekzander <alexandrepierret@protonmail.com>
2025-07-03 14:28:47 +0200

    feat: implement environment array creation

--- src/execution/execute_cmd_node.c
+++ src/execution/execute_cmd_node.c
@@ -6,21 +6,40 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 18:32:53 by apierret          #+#    #+#             */
-/*   Updated: 2025/07/03 12:01:37 by apierret         ###   ########.fr       */
+/*   Updated: 2025/07/03 14:28:05 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+#include "env.h"
 #include "execution.h"
 #include "expand.h"
 #include "lexer.h"
 #include "utils.h"
 
+static t_error	prepare_envp(char ***envp, t_hash_table *env)
+{
+	char	**arr;
+	char	*str;
+	t_error	err;
+
+	err = get_env(&str, env);
+	if (err.id != ERR_NONE)
+		return (err);
+	arr = ft_split(str, '\n');
+	if (arr == NULL)
+		return (free(str), err);
+	free(str);
+	return (*envp = arr, error(ERR_NONE, NULL));
+}
+
 t_error	execute_cmd_node(t_ast *node, t_hash_table *env)
 {
-	t_error	err;
 	char	*executable;
-	char	**arr;
+	char	**args;
+	char	**envp;
+	t_error	err;
 
 	if (node == NULL || env == NULL)
 		return (error(ERR_IMPLEMENTATION, NULL));
@@ -31,10 +50,14 @@ t_error	execute_cmd_node(t_ast *node, t_hash_table *env)
 	err = find_executable(&executable, node->command_args->content, env);
 	if (err.id != ERR_NONE)
 		return (err);
-	err = prepare_arguments(&arr, node->command_args);
+	err = prepare_arguments(&args, node->command_args);
 	if (err.id != ERR_NONE)
 		return (free(executable), err);
+	err = prepare_envp(&envp, env);
+	if (err.id != ERR_NONE)
+		return (free(executable), free_ddarray((void **) args), err);
 	free(executable);
-	free_ddarray((void **) arr);
+	free_ddarray((void **) args);
+	free_ddarray((void **) envp);
 	return (error(ERR_NONE, NULL));
 }
