87f050fcb5628f1257c5790e462d00c40cac4c0a
Vlekzander <alexandrepierret@protonmail.com>
2025-07-05 01:15:30 +0200

    feat: add signal handling

--- src/env/get_var.c
+++ src/env/get_var.c
@@ -6,19 +6,32 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 15:16:09 by apierret          #+#    #+#             */
-/*   Updated: 2025/06/23 21:54:51 by apierret         ###   ########.fr       */
+/*   Updated: 2025/07/05 01:12:41 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
 #include "utils.h"
 
+extern int	g_signal;
+
 t_error	get_var(char **str, t_hash_table *env, char *key)
 {
 	char	*value;
+	char	sig_str[4];
+	t_error	err;
 
 	if (str == NULL || env == NULL || key == NULL)
 		return (error(ERR_IMPLEMENTATION, NULL));
+	if (g_signal != 0)
+	{
+		sig_str[0] = '\0';
+		ft_strlcat(sig_str, "130", 4);
+		g_signal = 0;
+		err = set_var(env, "?", sig_str);
+		if (err.id != ERR_NONE)
+			return (err);
+	}
 	value = htable_get(env, key);
 	if (value == NULL)
 		value = "";
--- src/execution/get_exit_code.c
+++ src/execution/get_exit_code.c
@@ -6,30 +6,37 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 21:33:34 by apierret          #+#    #+#             */
-/*   Updated: 2025/07/04 21:35:16 by apierret         ###   ########.fr       */
+/*   Updated: 2025/07/05 01:14:41 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/wait.h>
 #include "execution.h"
 
+extern int	g_signal;
+
 int	get_exit_code(pid_t pid, t_error err)
 {
 	int	status;
 
-	status = 0;
 	if (pid != -1)
 	{
 		waitpid(pid, &status, 0);
 		if (WIFEXITED(status))
 			status = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+		{
+			g_signal = 0;
+			status = 128 + WTERMSIG(status);
+		}
 		return (status);
 	}
+	status = 1;
 	if (err.id == ERR_NONE)
-		return (0);
-	if (err.id == ERR_PERMISSION)
-		return (126);
-	if (err.id == ERR_FILE_NOT_FOUND || err.id == ERR_CMD_NOT_FOUND)
-		return (127);
-	return (1);
+		status = 0;
+	else if (err.id == ERR_PERMISSION)
+		status = 126;
+	else if (err.id == ERR_FILE_NOT_FOUND || err.id == ERR_CMD_NOT_FOUND)
+		status = 127;
+	return (status);
 }
