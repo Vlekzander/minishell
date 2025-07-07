e2f91b77d99f68ce59d21ba743f771d95ccda0af
Vlekzander <alexandrepierret@protonmail.com>
2025-07-07 21:00:30 +0200

    feat: implement env builtin command

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 15:22:25 by apierret          #+#    #+#             */
-/*   Updated: 2025/07/07 19:10:16 by apierret         ###   ########.fr       */
+/*   Updated: 2025/07/07 20:56:55 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ typedef t_error	(*t_btin)(int *ret, t_btin_data data, t_hash_table *env);
 
 t_btin		get_builtin(char *name);
 t_error		builtin_echo(int *ret, t_btin_data data, t_hash_table *env);
+t_error		builtin_env(int *ret, t_btin_data data, t_hash_table *env);
 t_error		builtin_exit(int *ret, t_btin_data data, t_hash_table *env);
 t_error		builtin_pwd(int *ret, t_btin_data data, t_hash_table *env);
 
--- src/builtins/builtin_env.c
+++ src/builtins/builtin_env.c
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_builtin.c                                      :+:      :+:    :+:   */
+/*   builtin_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/06 15:22:48 by apierret          #+#    #+#             */
-/*   Updated: 2025/07/07 20:57:25 by apierret         ###   ########.fr       */
+/*   Created: 2025/07/07 20:49:27 by apierret          #+#    #+#             */
+/*   Updated: 2025/07/07 20:58:35 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-
+#include <stdlib.h>
 #include "builtins.h"
-#include "libft.h"
+#include "data.h"
+#include "env.h"
 
-t_btin	get_builtin(char *name)
+t_error	builtin_env(int *ret, t_btin_data data, t_hash_table *env)
 {
-	if (name == NULL)
-		return (NULL);
-	if (ft_strncmp("echo", name, 5) == 0)
-		return (builtin_echo);
-	if (ft_strncmp("env", name, 4) == 0)
-		return (builtin_env);
-	if (ft_strncmp("exit", name, 5) == 0)
-		return (builtin_exit);
-	if (ft_strncmp("pwd", name, 4) == 0)
-		return (builtin_pwd);
-	return (NULL);
+	t_error	err;
+	char	*str;
+
+	if (ret == NULL || data.argv == NULL || env == NULL)
+		return (error(ERR_IMPLEMENTATION, NULL));
+	err = get_env(&str, env);
+	if (err.id != ERR_NONE)
+		return (*ret = 1, err);
+	ft_putstr_fd(str, data.stdout);
+	free(str);
+	return (*ret = 0, error(ERR_NONE, NULL));
 }
