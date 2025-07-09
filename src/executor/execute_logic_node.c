e8cfd35506ed01519b5225f339a5d17e79199a4b
Vlekzander <alexandrepierret@protonmail.com>
2025-07-10 00:36:28 +0200

    refactor: Update logical operators (&& ||) execution to match bash behavior

--- src/executor/execute_logic_node.c
+++ src/executor/execute_logic_node.c
@@ -6,7 +6,7 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 18:32:53 by apierret          #+#    #+#             */
-/*   Updated: 2025/07/03 23:46:13 by apierret         ###   ########.fr       */
+/*   Updated: 2025/07/10 00:33:44 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,13 @@ t_error	execute_logic_node(t_ast *node, t_hash_table *env)
 		|| (node->type != NODE_AND && node->type != NODE_OR))
 		return (error(ERR_IMPLEMENTATION, NULL));
 	err = execute_node(node->left, env);
+	node->exit_code = node->left->exit_code;
 	if (err.id != ERR_NONE)
 		return (err);
-	if ((node->type == NODE_AND && node->left->exit_code != 0)
-		|| (node->type == NODE_OR && node->left->exit_code == 0))
-		return (node->exit_code = node->left->exit_code, err);
+	if ((node->type == NODE_AND && node->exit_code != 0)
+		|| (node->type == NODE_OR && node->exit_code == 0))
+		return (err);
 	err = execute_node(node->right, env);
-	if (err.id != ERR_NONE)
-		return (err);
 	node->exit_code = node->right->exit_code;
-	return (error(ERR_NONE, NULL));
+	return (err);
 }
