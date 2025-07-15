4cbc27c7d82afe0585d3ba2ea06d0f91e4cf52f5
Vlekzander <alexandrepierret@protonmail.com>
2025-07-15 17:50:45 +0200

    fix: repair globbing that was not consuming checked parts

--- src/expand/globbing.c
+++ src/expand/globbing.c
@@ -6,7 +6,7 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 23:20:14 by apierret          #+#    #+#             */
-/*   Updated: 2025/07/01 21:50:40 by apierret         ###   ########.fr       */
+/*   Updated: 2025/07/15 17:49:19 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,76 @@
 #include "expand.h"
 #include "utils.h"
 
-static void	check_pattern(t_list *node, t_pattern *pattern)
+char	*ft_strrstr(char *str, char *to_find)
+{
+	char	*last;
+	char	*ptr;
+
+	if (str == NULL || to_find == NULL)
+		return (NULL);
+	last = NULL;
+	ptr = ft_strstr(str, to_find);
+	while (ptr != NULL)
+	{
+		last = ptr;
+		ptr++;
+		ptr = ft_strstr(ptr, to_find);
+	}
+	return (last);
+}
+
+static int	check_infixes(char **str, t_pattern *pattern)
 {
 	t_list	*infixe;
 	char	*ptr;
 
-	if (node == NULL || pattern == NULL)
-		return ;
-	ptr = node->content;
-	if (pattern->prefix != NULL)
-		ptr = ft_strstr(ptr, pattern->prefix);
-	if ((pattern->prefix != NULL && ptr != node->content)
-		|| (pattern->prefix == NULL && ptr[0] == '.'))
-		((char *) node->content)[0] = '\0';
-	if (pattern->infixes != NULL && ptr != NULL)
+	if (str == NULL || pattern == NULL)
+		return (-1);
+	ptr = *str;
+	if (pattern->infixes != NULL)
 	{
 		infixe = pattern->infixes;
 		while (infixe != NULL && ptr != NULL)
 		{
 			ptr = ft_strstr(ptr, infixe->content);
+			if (ptr != NULL)
+				ptr += ft_strlen(infixe->content);
 			infixe = infixe->next;
 		}
+		if (ptr == NULL)
+			return (0);
+		*str = ptr;
 	}
-	if (pattern->suffix != NULL && ptr != NULL)
-		ptr = ft_strstr(ptr, pattern->suffix);
-	if (ptr == NULL || (pattern->suffix != NULL
-			&& ft_strlen(ptr) != ft_strlen(pattern->suffix)))
-		((char *) node->content)[0] = '\0';
+	return (1);
+}
+
+static int	check_pattern(char *str, t_pattern *pattern)
+{
+	char	*ptr;
+	int		ret;
+
+	if (str == NULL || pattern == NULL)
+		return (-1);
+	if (pattern->prefix == NULL && str[0] == '.')
+		return (0);
+	ptr = str;
+	if (pattern->prefix != NULL)
+	{
+		ptr = ft_strstr(ptr, pattern->prefix);
+		if (ptr != str)
+			return (0);
+		ptr += ft_strlen(pattern->prefix);
+	}
+	ret = check_infixes(&ptr, pattern);
+	if (ret != 1)
+		return (ret);
+	if (pattern->suffix != NULL)
+	{
+		ptr = ft_strrstr(ptr, pattern->suffix);
+		if (ptr == NULL || ft_strlen(ptr) != ft_strlen(pattern->suffix))
+			return (0);
+	}
+	return (1);
 }
 
 static t_error	lst_dup(t_list **dest, t_list *base)
@@ -67,21 +110,28 @@ static t_error	lst_dup(t_list **dest, t_list *base)
 
 t_error	globbing(t_list **out_files, t_list *in_files, t_pattern *pattern)
 {
+	t_list	*lst;
 	t_list	*node;
 	t_error	err;
+	int		ret;
 
 	if (out_files == NULL || pattern == NULL)
 		return (error(ERR_IMPLEMENTATION, NULL));
-	*out_files = NULL;
-	err = lst_dup(out_files, in_files);
+	lst = NULL;
+	err = lst_dup(&lst, in_files);
 	if (err.id != ERR_NONE)
 		return (err);
-	node = *out_files;
+	node = lst;
 	while (node != NULL)
 	{
-		check_pattern(node, pattern);
+		ret = check_pattern(node->content, pattern);
+		if (ret == -1)
+			return (ft_lstclear(&lst, free), error(ERR_IMPLEMENTATION, NULL));
+		if (ret == 0)
+			((char *) node->content)[0] = '\0';
 		node = node->next;
 	}
-	lst_remove(out_files, str_empty, free);
+	lst_remove(&lst, str_empty, free);
+	*out_files = lst;
 	return (error(ERR_NONE, NULL));
 }
