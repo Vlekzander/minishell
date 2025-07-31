789a50e772d4771a0b0c9bd81a5fb8c01ffb8bae
Vlekzander <alexandrepierret@protonmail.com>
2025-07-31 20:14:51 +0200

    fix: Add default error exit code

--- src/executor/get_exit_code.c
+++ src/executor/get_exit_code.c
@@ -6,7 +6,7 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 21:33:34 by apierret          #+#    #+#             */
-/*   Updated: 2025/07/16 13:03:52 by apierret         ###   ########.fr       */
+/*   Updated: 2025/07/31 20:13:56 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ static int	exit_code_err(t_ret ret, t_error err)
 		exit_code = 126;
 	else if (err.id == ERR_FILE_NOT_FOUND || err.id == ERR_CMD_NOT_FOUND)
 		exit_code = 127;
+	else if (err.id != ERR_NONE)
+		return (1);
 	return (exit_code);
 }
 
