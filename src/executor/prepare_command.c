5d316ba0409e32f97acc720afc16ef490987a6fc
Vlekzander <alexandrepierret@protonmail.com>
2025-08-03 16:32:25 +0200

    refactor: parse env string by iteration to prevent issues when values contain newlines

--- src/executor/prepare_command.c
+++ src/executor/prepare_command.c
@@ -6,7 +6,7 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 14:18:14 by apierret          #+#    #+#             */
-/*   Updated: 2025/08/02 18:59:39 by apierret         ###   ########.fr       */
+/*   Updated: 2025/08/03 16:29:36 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,50 @@
 #include "executor.h"
 #include "libft.h"
 #include "utils.h"
+
+static int	add_env_to_lst(char *key, void *value, void *data)
+{
+	t_list			**lst;
+	size_t			len;
+	char			*str;
+	t_list			*node;
+
+	if (key == NULL || data == NULL || data + 1 == NULL)
+		return (0);
+	if (value == NULL || ft_strncmp("?", key, 2) == 0)
+		return (1);
+	lst = data;
+	len = ft_strlen(key) + ft_strlen(value) +1;
+	str = ft_calloc(len + 1, sizeof(char));
+	if (str == NULL)
+		return (0);
+	node = ft_lstnew(str);
+	if (node == NULL)
+		return (free(str), 0);
+	ft_strlcat(str, key, len + 1);
+	ft_strlcat(str, "=", len + 1);
+	ft_strlcat(str, value, len + 1);
+	ft_lstadd_back(lst, node);
+	return (1);
+}
+
+static t_error	prepare_envp(char ***envp, t_hash_table *env)
+{
+	char	**array;
+	t_list	*lst;
+	t_error	err;
+
+	if (envp == NULL || env == NULL)
+		return (error(ERR_IMPLEMENTATION, NULL));
+	lst = NULL;
+	if (!htable_foreach(env, add_env_to_lst, &lst))
+		return (ft_lstclear(&lst, free), error(ERR_ALLOCATION, NULL));
+	err = lst_array(&array, lst);
+	if (err.id != ERR_NONE)
+		return (ft_lstclear(&lst, free), err);
+	ft_lstclear(&lst, free);
+	return (*envp = array, error(ERR_NONE, NULL));
+}
 
 static t_error	prepare_arguments(char ***argv, int *argc, t_list *cmd_args)
 {
@@ -37,24 +81,6 @@ static t_error	prepare_arguments(char ***argv, int *argc, t_list *cmd_args)
 	*argv = arr;
 	*argc = ft_lstsize(cmd_args);
 	return (error(ERR_NONE, NULL));
-}
-
-static t_error	prepare_envp(char ***envp, t_hash_table *env)
-{
-	char	**arr;
-	char	*str;
-	t_error	err;
-
-	if (envp == NULL || env == NULL)
-		return (error(ERR_IMPLEMENTATION, NULL));
-	err = get_env(&str, 0, env);
-	if (err.id != ERR_NONE)
-		return (err);
-	arr = ft_split(str, '\n');
-	if (arr == NULL)
-		return (free(str), err);
-	free(str);
-	return (*envp = arr, error(ERR_NONE, NULL));
 }
 
 static t_error	prepare_binary(t_command *cmd, t_list *args, t_hash_table *env)
